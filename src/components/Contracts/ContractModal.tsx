1e6adc4d7ba6f1e4da09f9eccf78348e4b67123f
cha9liba <cha9liba1@gmail.com>
2026-04-05 10:33:38 +0100

    fix useEffect import in ContractModal

--- src/components/Contracts/ContractModal.tsx
+++ src/components/Contracts/ContractModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { X, Eye, History } from "lucide-react";
 import { useForm, type SubmitHandler } from "react-hook-form";
